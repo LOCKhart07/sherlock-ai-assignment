2ae96aa286e0c01b055d3ecbbdb1dff903c7b470
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-04-10 11:27:07 +0530

    Update Docker configuration and database path
    - Added volume mapping in docker-compose.yaml to persist data in /mnt/sherlock-ai.
    - Updated SQLALCHEMY_DATABASE_URL in config.py to point to the new database location at /mnt/auth.db.

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,7 +10,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Database Configuration
-SQLALCHEMY_DATABASE_URL = "sqlite:///./auth.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////mnt/auth.db"
 
 # Google OAuth2 Configuration
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
