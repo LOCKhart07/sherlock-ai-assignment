d5f073a792b6e679c8d1e8053b08ee8504154b2e
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-04-10 14:36:57 +0530

    Add debug print statement for SQLALCHEMY_DATABASE_URL in session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from core.config import SQLALCHEMY_DATABASE_URL
 
+print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
